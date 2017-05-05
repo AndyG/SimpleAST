8a88ffd8001d26cf4e13b648cb3c9009777ce21a
andygarron <anderson.garron@gmail.com>
2017-05-04 21:37:57 -0700

    made SpannableRenderableParent abstract

--- simpleast-core/src/main/java/com/agarron/simpleast_core/simple/SpannableRenderableParent.java
+++ simpleast-core/src/main/java/com/agarron/simpleast_core/simple/SpannableRenderableParent.java
@@ -9,16 +9,9 @@ import com.agarron.simpleast_core.node.Parent;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SpannableRenderableParent implements SpannableRenderable, Parent {
+public abstract class SpannableRenderableParent implements SpannableRenderable, Parent {
 
     protected final List<SpannableRenderable> children = new ArrayList<>();
-
-    public static final String TYPE = "parent";
-
-    @Override
-    public String getType() {
-        return TYPE;
-    }
 
     @Override
     public List getChildren() {
