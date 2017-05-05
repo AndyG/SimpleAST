76cac433b4598d65e564303096ae84eb40179a15
andygarron <anderson.garron@gmail.com>
2017-05-04 23:12:02 -0700

    removed abstract parent keyword

--- simpleast-core/src/main/java/com/agarron/simpleast_core/simple/SpannableRenderableParent.java
+++ simpleast-core/src/main/java/com/agarron/simpleast_core/simple/SpannableRenderableParent.java
@@ -9,9 +9,16 @@ import com.agarron.simpleast_core.node.Parent;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class SpannableRenderableParent implements SpannableRenderable, Parent {
+public class SpannableRenderableParent implements SpannableRenderable, Parent {
 
     protected final List<SpannableRenderable> children = new ArrayList<>();
+
+    public static final String TYPE = "parent";
+
+    @Override
+    public String getType() {
+        return TYPE;
+    }
 
     @Override
     public List getChildren() {
