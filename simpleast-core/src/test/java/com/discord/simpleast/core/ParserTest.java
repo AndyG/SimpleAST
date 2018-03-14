946cbd1022cdc7f4e915dafcd46d1b3ba83ffe87
andygarron <anderson.garron@gmail.com>
2018-03-14 16:17:14 -0700

    rc11
    Signed-off-by: andygarron <anderson.garron@gmail.com>

--- simpleast-core/src/test/java/com/discord/simpleast/core/ParserTest.java
+++ simpleast-core/src/test/java/com/discord/simpleast/core/ParserTest.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 public class ParserTest {
 
-    private Parser<SpannableRenderableNode<Object>> parser;
+    private Parser<Object, Node<Object>> parser;
     private TreeMatcher treeMatcher;
 
     @Before
@@ -41,13 +41,13 @@ public class ParserTest {
 
     @Test
     public void testEmptyParse() throws Exception {
-        final List<SpannableRenderableNode<Object>> ast = parser.parse("");
+        final List<Node<Object>> ast = parser.parse("");
         Assert.assertTrue(ast.isEmpty());
     }
 
     @Test
     public void testParseFormattedText() throws Exception {
-        final List<SpannableRenderableNode<Object>> ast = parser.parse("**bold**");
+        final List<Node<Object>> ast = parser.parse("**bold**");
 
         final StyleNode boldNode = StyleNode.Companion.createWithText("bold", Collections.singletonList((CharacterStyle) new StyleSpan(Typeface.BOLD)));
 
@@ -57,7 +57,7 @@ public class ParserTest {
 
     @Test
     public void testParseLeadingFormatting() throws Exception {
-        final List<SpannableRenderableNode<Object>> ast = parser.parse("**bold** and not bold");
+        final List<Node<Object>> ast = parser.parse("**bold** and not bold");
 
         final StyleNode boldNode = StyleNode.Companion.createWithText("bold", Collections.singletonList((CharacterStyle) new StyleSpan(Typeface.BOLD)));
         final TextNode trailingText = new TextNode(" and not bold");
@@ -68,7 +68,7 @@ public class ParserTest {
 
     @Test
     public void testParseTrailingFormatting() throws Exception {
-        final List<SpannableRenderableNode<Object>> ast = parser.parse("not bold **and bold**");
+        final List<Node<Object>> ast = parser.parse("not bold **and bold**");
 
         final TextNode leadingText = new TextNode("not bold ");
         final StyleNode boldNode = StyleNode.Companion.createWithText("and bold", Collections.singletonList((CharacterStyle) new StyleSpan(Typeface.BOLD)));
@@ -79,17 +79,17 @@ public class ParserTest {
 
     @Test
     public void testNestedFormatting() throws Exception {
-//        final List<SpannableRenderableNode> ast = parser.parse("*** test1 ** test2 * test3 * test4 ** test5 ***");
-        final List<SpannableRenderableNode<Object>> ast = parser.parse("**bold *and italics* and more bold**");
+//        final List<Node> ast = parser.parse("*** test1 ** test2 * test3 * test4 ** test5 ***");
+        final List<Node<Object>> ast = parser.parse("**bold *and italics* and more bold**");
 //        final List<Node> ast = parser.parse("______" +
 //            "t"
 //        + "______");
 
-        final StyleNode boldNode = new StyleNode(Collections.singletonList((CharacterStyle) new StyleSpan(Typeface.BOLD)));
-        boldNode.addChild(new TextNode("bold "));
+        final StyleNode<Object> boldNode = new StyleNode<>(Collections.singletonList((CharacterStyle) new StyleSpan(Typeface.BOLD)));
+        boldNode.addChild(new TextNode<>("bold "));
         boldNode.addChild(StyleNode.Companion.createWithText("and italics",
             Collections.singletonList((CharacterStyle) new StyleSpan(Typeface.ITALIC))));
-        boldNode.addChild(new TextNode(" and more bold"));
+        boldNode.addChild(new TextNode<>(" and more bold"));
 
         final List<? extends Node> model = Collections.singletonList(boldNode);
         Assert.assertTrue(treeMatcher.matches(model, ast));
